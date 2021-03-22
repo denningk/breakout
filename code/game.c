af5fe0aa2eb87b7c5513290c477286d3427a8b9f
Keith <keithdenning1@gmail.com>
2021-03-21 23:39:32 -0400

    Move rectangle

--- code/game.c
+++ code/game.c
@@ -1,8 +1,11 @@
+int p = 20;
+
 internal void SimulateGame(Input *input)
 {
     ClearScreen(0x551100);
     if (input->buttons[BUTTON_LEFT].isDown)
     {
-        DrawRectInPixels(20, 20, 50, 50, 0xffff00);
+        p += 20;
     }
+    DrawRectInPixels(p, 20, p + 30, 50, 0xffff00);
 }
